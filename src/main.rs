c88f4b119906f089f329e850857347d35c78be48
Justus Dieckmann <justusdieckmann@gmail.com>
2024-03-23 20:16:54 +0100

    First attempt at user interface

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ use std::collections::HashMap;
 use model_checker;
 
 use eframe::egui;
-use eframe::egui::{Align2, Color32, FontDefinitions, FontId, Frame, PointerButton, Sense, Shape};
+use eframe::egui::{Align2, Color32, FontId, Frame, PointerButton, Sense, Shape, Ui};
 use eframe::emath::{Pos2, Vec2};
-use eframe::epaint::{Stroke, TextShape};
+use eframe::epaint::{Stroke};
+
+type StateId = u64;
 
 fn main() -> Result<(), eframe::Error> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
@@ -19,7 +21,7 @@ fn main() -> Result<(), eframe::Error> {
     eframe::run_native(
         "Model-Checker",
         options,
-        Box::new(|cc| {
+        Box::new(|_cc| {
             Box::<MyApp>::default()
         }),
     )
@@ -38,15 +40,15 @@ const ARROWHEAD_LENGTH: f32 = 12.0;
 struct MyState {
     aps: Vec<String>,
     pos: Pos2,
-    id: u64,
+    id: StateId,
 }
 
 struct MyApp {
-    states: HashMap<u64, MyState>,
-    transitions: Vec<(u64, u64)>,
-    current_id: u64,
-    start_drag: Option<u64>,
-    selected_id: Option<u64>,
+    states: HashMap<StateId, MyState>,
+    transitions: Vec<(StateId, StateId)>,
+    current_id: StateId,
+    start_drag: Option<StateId>,
+    selected_id: Option<StateId>,
     query: String,
     aptext: String,
 }
@@ -65,120 +67,129 @@ impl Default for MyApp {
     }
 }
 
+impl MyApp {
+
+    fn my_update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame, ui: &mut Ui) {
+        ui.horizontal(|ui| {
+            let name_label = ui.label("Query: ");
+            ui.text_edit_singleline(&mut self.query)
+                .labelled_by(name_label.id);
+            let button = ui.button("Check");
+            if button.clicked() {
+                // ...
+            }
+        });
+        ui.horizontal(|ui| {
+            let name_label = ui.label("APs: ");
+            ui.text_edit_singleline(&mut self.aptext)
+                .labelled_by(name_label.id);
+            let button = ui.button("Set");
+            if button.clicked() {
+                if let Some(selected_id) = self.selected_id {
+                    let state = self.states.get_mut(&selected_id).unwrap();
+                    state.aps = self.aptext.split(",").filter(|s| { !s.trim().is_empty() })
+                        .map(|a| {
+                            a.trim().to_owned()
+                        }).collect();
+                }
+            }
+        });
+
+        Frame::canvas(ui.style()).show(ui, |ui| {
+            let color = if ctx.style().visuals.dark_mode {
+                Color32::from_gray(200)
+            } else {
+                Color32::from_gray(25)
+            };
+
+            let (mut response, painter) =
+                ui.allocate_painter(ui.available_size_before_wrap(), Sense::click_and_drag());
+
+            if let Some(pointer_pos) = response.interact_pointer_pos() {
+                let canvas_pos = pointer_pos;
+
+                let result = self.states.values().find(|state| {
+                    state.pos.distance(canvas_pos) <= STATE_RADIUS
+                });
+                if let Some(hit_state) = result {
+                    if response.drag_started_by(PointerButton::Primary) {
+                        self.start_drag = Some(hit_state.id);
+                    } else if response.drag_released_by(PointerButton::Primary) {
+                        if let Some(start_drag) = self.start_drag {
+                            if !self.transitions.contains(&(start_drag, hit_state.id)) {
+                                self.transitions.push((start_drag, hit_state.id));
+                                response.mark_changed();
+                            }
+                        }
+                    } else if response.clicked() {
+                        self.selected_id = Some(hit_state.id);
+                        self.aptext = hit_state.aps.join(", ");
+                        response.mark_changed();
+                    }
+                } else {
+                    if response.clicked() {
+                        self.states.insert(self.current_id, MyState {
+                            aps: vec![],
+                            pos: canvas_pos,
+                            id: self.current_id,
+                        });
+                        self.selected_id = Some(self.current_id);
+                        self.aptext.clear();
+                        self.current_id += 1;
+                        response.mark_changed();
+                    }
+                }
+            } else {
+                if response.drag_started_by(PointerButton::Primary) || response.drag_released_by(PointerButton::Primary) {
+                    self.start_drag = None;
+                }
+            }
+
+
+            let shapes = ctx.fonts(|f| {
+                let mut shapes = vec![];
+                for state in self.states.values() {
+                    let stroke_color = if self.selected_id == Some(state.id) {
+                        Color32::from_rgb(0, 127, 255)
+                    } else {
+                        color
+                    };
+
+                    shapes.push(Shape::circle_stroke(state.pos, 20.0, Stroke::new(4.0, stroke_color)));
+                    shapes.push(Shape::text(f, state.pos - Vec2 { x: 25.0, y: 0.0 }, Align2::RIGHT_CENTER, state.aps.join("\n"), FontId::monospace(12.0), color));
+                }
+                shapes
+            });
+
+            painter.extend(shapes);
+
+            painter.extend(self.transitions.iter()
+                .flat_map(|(id1, id2)| {
+                    let state1 = self.states.get(id1).unwrap();
+                    let state2 = self.states.get(id2).unwrap();
+                    let dir = (state2.pos - state1.pos).normalized();
+                    let cross_dir = dir.rot90();
+                    let arrow_basepoint = state2.pos - dir * (STATE_RADIUS + ARROWHEAD_LENGTH);
+                    return [
+                        Shape::convex_polygon(vec![
+                            state2.pos - dir * STATE_RADIUS,
+                            arrow_basepoint + cross_dir * ARROWHEAD_HALF_WIDTH,
+                            arrow_basepoint - cross_dir * ARROWHEAD_HALF_WIDTH,
+                        ], color, Stroke::NONE),
+                        Shape::line_segment([state1.pos + dir * STATE_RADIUS, arrow_basepoint], Stroke::new(2.0, color)),
+                    ];
+                }));
+
+            response
+        });
+    }
+
+}
+
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Query: ");
-                ui.text_edit_singleline(&mut self.query)
-                    .labelled_by(name_label.id);
-                let button = ui.button("Check");
-                if button.clicked() {
-                    // ...
-                }
-            });
-            ui.horizontal(|ui| {
-                let name_label = ui.label("APs: ");
-                ui.text_edit_singleline(&mut self.aptext)
-                    .labelled_by(name_label.id);
-                let button = ui.button("Set");
-                if button.clicked() {
-                    if let Some(selected_id) = self.selected_id {
-                        let state = self.states.get_mut(&selected_id).unwrap();
-                        state.aps = self.aptext.split(",").filter(|s| {!s.trim().is_empty()})
-                            .map(|a| {
-                            a.trim().to_owned()
-                        }).collect();
-                    }
-                }
-            });
-
-            Frame::canvas(ui.style()).show(ui, |ui| {
-                let color = if ctx.style().visuals.dark_mode {
-                    Color32::from_gray(200)
-                } else {
-                    Color32::from_gray(25)
-                };
-
-
-                let (mut response, painter) =
-                    ui.allocate_painter(ui.available_size_before_wrap(), Sense::click_and_drag());
-
-                if let Some(pointer_pos) = response.interact_pointer_pos() {
-                    let canvas_pos = pointer_pos;
-
-                    let result = self.states.values().find(|state| {
-                        state.pos.distance(canvas_pos) <= STATE_RADIUS
-                    });
-                    if let Some(hit_state) = result {
-                        if response.drag_started_by(PointerButton::Primary) {
-                            self.start_drag = Some(hit_state.id);
-                        } else if response.drag_released_by(PointerButton::Primary) {
-                            if let Some(start_drag) = self.start_drag {
-                                if !self.transitions.contains(&(start_drag, hit_state.id)) {
-                                    self.transitions.push((start_drag, hit_state.id));
-                                    response.mark_changed();
-                                }
-                            }
-                        } else if response.clicked() {
-                            self.selected_id = Option::from(hit_state.id);
-                            response.mark_changed();
-                        }
-                    } else {
-                        if response.clicked() {
-                            self.states.insert(self.current_id, MyState {
-                                aps: vec![],
-                                pos: canvas_pos,
-                                id: self.current_id,
-                            });
-                            self.selected_id = Some(self.current_id);
-                            self.current_id += 1;
-                            response.mark_changed();
-                        }
-                    }
-                } else {
-                    if response.drag_started_by(PointerButton::Primary) || response.drag_released_by(PointerButton::Primary) {
-                        self.start_drag = None;
-                    }
-                }
-
-                let shapes = self.states.values()
-                    .flat_map(|state| {
-                        let stroke_color = if self.selected_id == Some(state.id) {
-                            Color32::from_rgb(0, 127, 255)
-                        } else {
-                            color
-                        };
-                        let shape = ctx.fonts(|f| {
-                            Shape::text(f, state.pos - Vec2 { x: 120.0, y: 0.0 }, Align2::RIGHT_CENTER, "", FontId::monospace(12.0), color)
-                        });
-                        [
-                            Shape::circle_stroke(state.pos, 20.0, Stroke::new(4.0, stroke_color)),
-                            shape
-                        ]
-                    });
-
-                painter.extend(shapes);
-
-                painter.extend(self.transitions.iter()
-                    .flat_map(|(id1, id2)| {
-                        let state1 = self.states.get(id1).unwrap();
-                        let state2 = self.states.get(id2).unwrap();
-                        let dir = (state2.pos - state1.pos).normalized();
-                        let cross_dir = dir.rot90();
-                        let arrow_basepoint = state2.pos - dir * (STATE_RADIUS + ARROWHEAD_LENGTH);
-                        return [
-                            Shape::convex_polygon(vec![
-                                state2.pos - dir * STATE_RADIUS,
-                                arrow_basepoint + cross_dir * ARROWHEAD_HALF_WIDTH,
-                                arrow_basepoint - cross_dir * ARROWHEAD_HALF_WIDTH,
-                            ], color, Stroke::NONE),
-                            Shape::line_segment([state1.pos + dir * STATE_RADIUS, arrow_basepoint], Stroke::new(2.0, color)),
-                        ];
-                    }));
-
-                response
-            });
+            self.my_update(ctx, _frame, ui);
         });
     }
 }
